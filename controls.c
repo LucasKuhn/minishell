1d7f9305924ac592d7f34b0505ac437fc8b92f56
sgkhusal <sheela.khusal.08@gmail.com>
2022-05-11 00:06:28 -0300

    Add some information about the problem handling SIGINT in the child

--- controls.c
+++ controls.c
@@ -6,16 +6,16 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 14:18:39 by sguilher          #+#    #+#             */
-/*   Updated: 2022/05/10 19:23:00 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/05/11 00:05:20 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	handle_signal(int sign)
+void	handle_signal(int sig)
 {
 	//ft_putstr_fd("Chegamos aqui\n", 1);
-	if (sign == SIGINT)
+	if (sig == SIGINT)
 	{
 		ft_putstr_fd("\n", STDOUT_FILENO);
 		ft_putstr_fd("Sou o pai\n", STDOUT_FILENO);
@@ -39,13 +39,14 @@ void	signals()
 	// signal(SIGQUIT, handle_signal); // ctrl+barra
 }
 
-void	handle_child_signal(int sign)
+void	handle_child_signal(int sig)
 {
-	if (sign == SIGINT)
+	if (sig == SIGINT)
 	{
 		dprintf(2, "oi\n");
 		rl_replace_line("", 0);
-		exit(142);
+		exit(10); // KILL + SIGINT
+		//independente do número no exit o status do waitpid = 2 e WIFEXITED não retorna true...
 	}
 	// else if (sign == SIGQUIT)
 	// 	exit(EXIT_SUCCESS);
@@ -62,6 +63,6 @@ void	child_signals(int child_pid)
 	if (child_pid == 0)
 		sa.sa_handler = &handle_child_signal;
 	else
-		sa.sa_handler = SIG_IGN;
+		sa.sa_handler = SIG_IGN; // SIG_IGN = ignorar o Ctrl+C no pai
 	sigaction(SIGINT, &sa, NULL); // ctrl+C
 }
