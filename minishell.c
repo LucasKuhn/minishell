958dfc13a454e64e584986dafa1430c48d478368
Lucas Kuhn <hi@lucaskuhn.dev>
2022-05-23 17:36:36 -0300

    move ctr+D handling to prompt_input

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: lalex-ku <lalex-ku@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 14:44:34 by lalex-ku          #+#    #+#             */
-/*   Updated: 2022/05/23 16:43:46 by lalex-ku         ###   ########.fr       */
+/*   Updated: 2022/05/23 17:29:19 by lalex-ku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,9 @@ int minishell(t_env	*minienv)
 	{
 		define_main_signals();
 		input = prompt_input(minienv);
-		if (!input)
-			builtin_exit(); // TODO: limpar quando acionar a exit (Ctrl+D)
-		if (!*input)
+		if (!*input || has_unclosed_quotes(input))
 			continue;
-		if (has_unclosed_quotes(input))
-			continue;
+		// TODO: check syntax before splitting
 		commands = split_commands(input); //TODO: limpar commands
 		if (commands[1] == NULL)
 			exit_status = execute_one_command(commands[0], &minienv);
